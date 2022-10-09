1558f6df6fddc7a5fda74ce9c36baf85e48078ee
xing-6464 <xing_6464@foxmail.com>
2022-10-09 21:12:17 +0800

    目录修改

--- main.ts
+++ main.ts
@@ -1,11 +1,6 @@
 import List from "./DataStructure/List/List"
 
 function main (): void {
-    const list = new List<number>()
-
-    list.addLast(1)
-    list.addLast(2)
-    console.log(list.get(1))
 }
 
 main()
